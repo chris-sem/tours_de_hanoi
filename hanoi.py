413a352df4913f9cb245283ca2ec0c9fd5ab94f3
chis-sem <semjeanchristopher4@gmail.com>
2023-10-23 18:02:05 +0200

    Ajout de la fonction action_utlisateur, qui va recupérer les actions de l'utilisateur pour pouvoir avancer le jeu

--- hanoi.py
+++ hanoi.py
@@ -1,3 +1,40 @@
+
+def action_utilisateur():
+    # Saisie de la chaîne contenant les deux nombres séparés par des virgules
+    input_string = input("Entrez les numeros du piquet de depart, d'arrivée (ou 'q' pour quitter) séparés par des virgules : ")
+
+    if input_string.strip().lower() == 'q':
+        # L'utilisateur a entré "q" pour quitter
+        print("Vous avez choisi de quitter !")
+        return (0,0)
+    else:
+        # Divisez la chaîne en utilisant la virgule comme séparateur
+        numbers = input_string.split(',')
+
+        # Assurez-vous que nous avons exactement deux éléments
+        if len(numbers) != 2:
+            print("Veuillez entrer deux nombres séparés par une virgule ! ")
+            return (-1,-1)
+        else:
+            try:
+                # Convertissez les éléments en nombres (float ici, mais vous pouvez utiliser int si nécessaire)
+                num1 = int(numbers[0])
+                num2 = int(numbers[1])
+
+                # Vérifiez que les nombres sont compris entre 1 et 3
+                if 1 <= num1 <= 3 and 1 <= num2 <= 3:
+                    # Les nombres sont valides
+                    #print("Premier nombre :", num1)
+                    #print("Deuxième nombre :", num2)
+                    return (num1, num2)
+                else:
+                    print("Les nombres doivent être compris entre 1 et 3.")
+                    return (-1,-1)
+            except ValueError:
+                print("Assurez-vous que les valeurs entrées sont des nombres valides.")
+                return (-1,-1)
+
+
 def nbre_symbole(n):
     return (5 + (n - 1) * 8)
 
@@ -22,6 +59,36 @@ class TourHanoi:
         self.piquet1.append(niveau+1)
         self.piquet2.append(niveau+1)
         self.piquet3.append(niveau+1)
+
+    def dessine_piquet(self):
+        print("\n",((int)((self.max_symbole - 1) / 2) + self.espace_init) * " ", "!", ((int)((self.max_symbole - 1) / 2) + self.espace_init) * " ", end=" ")
+        print("",((int)((self.max_symbole - 1) / 2) + self.espace_init) * " ", "!", ((int)((self.max_symbole - 1) / 2) + self.espace_init) * " ", end=" ")
+        print("",((int)((self.max_symbole - 1) / 2) + self.espace_init) * " ", "!", ((int)((self.max_symbole - 1) / 2) + self.espace_init) * " ", end=" \n")
+
+    def dessine_disque(self, niveau_disque):
+        if niveau_disque == 0 :
+            print("",((int)((self.max_symbole - 1) / 2) + self.espace_init ) * " ", "!", ((int)((self.max_symbole - 1) / 2) + self.espace_init) * " ", end=" ")
+        else :
+            n = nbre_symbole(niveau_disque)
+            nbre_espace = (int)((self.max_symbole - n) / 2) + self.espace_init
+            print("",nbre_espace * " ", n * "=", nbre_espace * " ", end=" ")
+
+    def dessine_socle(self):
+        print(((3 * (self.max_symbole + (2 * self.espace_init) + 4) - 2)) * ":")
+        print("+", (self.max_symbole + (2 * self.espace_init))*" ","+", (self.max_symbole + + (2 * self.espace_init))*" ","+", (self.max_symbole + + (2 * self.espace_init))*" ","+")
+        print(((3 * (self.max_symbole + (2 * self.espace_init) + 4) - 2)) * ":")
+
+    def dessine_jeu(self):
+
+        self.dessine_piquet()
+
+        for i in range(self.niveau):
+            self.dessine_disque(self.piquet1[i])
+            self.dessine_disque(self.piquet2[i])
+            self.dessine_disque(self.piquet3[i])
+            self.dessine_piquet()
+
+        self.dessine_socle()
 
     def deplacer_disque(self, num_piquet_depart, num_piquet_arrivee):
         indice_disque_a_prendre = 0
@@ -152,33 +219,3 @@ class TourHanoi:
             else:
                 print("Mouvement interdit !")
                 return 1
-
-    def dessine_piquet(self):
-        print("\n",((int)((self.max_symbole - 1) / 2) + self.espace_init) * " ", "!", ((int)((self.max_symbole - 1) / 2) + self.espace_init) * " ", end=" ")
-        print("",((int)((self.max_symbole - 1) / 2) + self.espace_init) * " ", "!", ((int)((self.max_symbole - 1) / 2) + self.espace_init) * " ", end=" ")
-        print("",((int)((self.max_symbole - 1) / 2) + self.espace_init) * " ", "!", ((int)((self.max_symbole - 1) / 2) + self.espace_init) * " ", end=" \n")
-
-    def dessine_disque(self, niveau_disque):
-        if niveau_disque == 0 :
-            print("",((int)((self.max_symbole - 1) / 2) + self.espace_init ) * " ", "!", ((int)((self.max_symbole - 1) / 2) + self.espace_init) * " ", end=" ")
-        else :
-            n = nbre_symbole(niveau_disque)
-            nbre_espace = (int)((self.max_symbole - n) / 2) + self.espace_init
-            print("",nbre_espace * " ", n * "=", nbre_espace * " ", end=" ")
-
-    def dessine_socle(self):
-        print(((3 * (self.max_symbole + (2 * self.espace_init) + 4) - 2)) * ":")
-        print("+", (self.max_symbole + (2 * self.espace_init))*" ","+", (self.max_symbole + + (2 * self.espace_init))*" ","+", (self.max_symbole + + (2 * self.espace_init))*" ","+")
-        print(((3 * (self.max_symbole + (2 * self.espace_init) + 4) - 2)) * ":")
-
-    def dessine_jeu(self):
-
-        self.dessine_piquet()
-
-        for i in range(self.niveau):
-            self.dessine_disque(self.piquet1[i])
-            self.dessine_disque(self.piquet2[i])
-            self.dessine_disque(self.piquet3[i])
-            self.dessine_piquet()
-
-        self.dessine_socle()
